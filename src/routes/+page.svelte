9077c30676f80a72e225ded583fc6bbced3790c5
DenysChernenko <67716495+DenysChernenko@users.noreply.github.com>
2023-04-06 14:03:38 -0400

    Added max moment reporting
    Added max moment reporting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,53 @@
 <script lang="ts">
 	import P5 from 'p5-svelte';
 
+    var ileng:number =20;
+    var iudl:number =20;
+
+    let maxMoment:number = 0;
+
+    var lengmin:number = 1;
+    var lengmax:number = 100
 
     let udl:number = 10;
     let leng:number = 20;
     let steps:number = 50;
     const inc:number = leng/steps;
+
+    let mx:number;
+    let my:number;
+
+    let curMouseX:number = 0;
+
+
+
+    function reportMouseLoc(arr:number[],curMouseX:number){
+        arr.sort((a,b) => {
+            return Math.abs(curMouseX-a) - Math.abs(curMouseX - b);
+        })
+        console.log(arr[0]);
+        return arr[0];
+    }
+
+
+    function pushLeng(ileng,lengmin){
+        if (ileng >= lengmin && ileng <= lengmax){
+        leng = ileng;
+        
+    };
+    }
+
+    function pushMaxMoment(iudl){
+        udl = iudl;
+        // console.log("udl is: " + udl);
+        // console.log("leng is: " + leng);
+        maxMoment = (udl*(leng*leng))/8;
+        maxMoment = Math.round(maxMoment*100)/100
+        // console.log("Max moment is: " + maxMoment)
+    };
+
+    
+
 
     function momentList(udl:number,leng:number, steps:number){
         let xVals = [];
@@ -19,16 +61,18 @@
             xVals.push(current);
             yVals.push(momentAtPoint(udl,current,leng));
             current = current+inc;
-            console.log(current)
+            // console.log(current)
         }
         let mlist = [xVals,yVals];
         return mlist;
     }
 
+
+
     let moments = momentList(udl,leng,steps);
 
-    console.log('inc is: ' + inc)
-    console.log(moments);
+    // console.log('inc is: ' + inc)
+    // console.log(moments);
 
     let canvX:number = 1200; // canvas x dimension
     let canvY:number = 600; // canvas y dimension
@@ -48,10 +92,10 @@
     };
 
     // console.log("baseline start point is: " + Baseline.start);
-    console.log("baseline end point is: " + Baseline.end);
-    console.log("baseline length is: " + Baseline.length);
+    // console.log("baseline end point is: " + Baseline.end);
+    // console.log("baseline length is: " + Baseline.length);
     // console.log("Baseline distance to canvas top is: " + dTCT);
-    console.log("Baseline origin is: " + Baseline.origin)
+    // console.log("Baseline origin is: " + Baseline.origin)
 
 
     function normalize(arr:number[],normVal:number){
@@ -74,18 +118,17 @@
     let momentX = relocate(normX,Baseline.origin[0]);
     let momentY = relocate(normY,Baseline.origin[1]);
 
-    console.log(normX,normY);
-    console.log(momentX,momentY);
+    // console.log(normX,normY);
+    // console.log(momentX,momentY);
 
     let fontSize = 30;
-    let fontOffset = 0*fontSize*1.25;
-
-
+    let fontOffset = fontSize*1.25;
+    let cnv:any;
 
 
     var overlay:any;
 
-	const sketch = (p5) => {
+	const sketch = (p5:any) => {
         p5.preload = () =>{
 
         };
@@ -93,7 +136,8 @@
 
 
             p5.pixelDensity(2);
-			p5.createCanvas(canvX, canvY);
+			cnv = p5.createCanvas(canvX, canvY);
+
             p5.background('white');
 
             overlay = p5.createGraphics(canvX,canvY);
@@ -114,13 +158,14 @@
             overlay.text('0',bl1[0],bl1[1] + fontOffset)
             overlay.textAlign('left')
             overlay.text(leng,bl2[0],bl2[1] + fontOffset)
-            p5.line(p5.mouseX,-pad,p5.mouseX,canvY) ;
+            p5.line(p5.mouseX,-pad,p5.mouseX,canvY);
             p5.strokeWeight(1);
             p5.noFill();
             p5.line(bl1[0],bl1[1],bl2[0],bl2[1]);
             p5.strokeWeight(2)
 
             for (let i=1; i <= steps; i++){
+                p5.line(momentX[i-1],momentY[i-1],momentX[i],momentY[i])
                 p5.line(momentX[i-1],momentY[i-1],momentX[i],momentY[i])
             };
 
@@ -147,16 +192,15 @@
         </div>
         <label>
             Length
-            <input type="number" bind:value={leng} id="length" min="1" max="200"/>
+            <input type="number" bind:value={ileng} on:input={pushLeng(ileng,lengmin),pushMaxMoment(iudl)} id="length" min="1" max="200"/>
 
         </label>
         
         <label>
             UDL
-            <input type="range" bind:value={udl} min="100" max="1000" step="0.01" />
-            {udl}
+            <input type="number" bind:value={iudl} on:input={pushMaxMoment(iudl)} min="0" max="1000" step="0.1" />
         </label>
-        
+        <div>Max Moment is: {maxMoment}</div>
     </div>
 
 </div>
